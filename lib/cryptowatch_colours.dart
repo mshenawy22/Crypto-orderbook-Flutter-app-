e5e76a4c23090639ab492a024a331db838edcffa
jay shenawy <jay.shenawy@netxtra.net>
2022-01-01 11:20:31 +0000

    defining App colours (as per the assignment pic)

---
+++ lib/cryptowatch_colours.dart
@@ -0,0 +1,17 @@
+import 'dart:ui';
+
+import 'dart:ui';
+
+const BLACKA  =const  Color(0xFF070711);   // for ex dividerColor
+
+const BLACKB = Color(0xFF42A5F5); // for ex scaffoldBackgroundColor
+
+const BLACKC =Color(0xFF2f2f47) ; // for ex input fields /button colours
+
+const BLACKD =Color(0xFF171721) ; // for ex order book bg colour
+
+const WHITEA = Color(0xFFe0e1e6); // white for text
+
+const GREENA = Color(0xFF748e71); // green for text(buy)
+
+const REDA = Color(0xFF835060);   //red for text (sell)
--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,7 @@ import 'package:cryptowatch_orderbook/ui/main_screen.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 
+import 'cryptowatch_colours.dart';
 import 'cubit/prodct_list_cubit/product_list_cubit.dart';
 
 void main() {
@@ -22,11 +23,29 @@ class MyApp extends StatelessWidget {
           ],
           child:
           MaterialApp(
-            title: 'Flutter Demo',
+            title:'CryptoWatch OrderBook',
             theme: ThemeData(
-              primarySwatch: Colors.blue,
+              // Define the default brightness and colors.
+              brightness: Brightness.dark,
+              dividerColor: BLACKA,
+              scaffoldBackgroundColor:  BLACKB,
+              canvasColor: BLACKD,
+              primaryTextTheme: const TextTheme(
+                headline1: TextStyle(fontSize: 72.0, fontWeight: FontWeight.bold),
+                headline6: TextStyle(fontSize: 36.0, fontStyle: FontStyle.italic),
+                headline4: TextStyle(fontSize: 14.0, fontStyle: FontStyle.italic),
+                bodyText2: TextStyle(fontSize: 14.0, fontFamily: 'Hind'),
+              ),
+
+              // Define the default font family.
+              fontFamily: 'Georgia',
               visualDensity: VisualDensity.adaptivePlatformDensity,
+                appBarTheme : AppBarTheme(titleTextStyle: TextStyle(fontSize: 25.0, fontWeight: FontWeight.bold)),
+              // Define the default `TextTheme`. Use this to specify the default
+              // text styling for headlines, titles, bodies of text, and more.
+
             ),
+
             home:
             MyHomePage(title: 'CryptoWatch OrderBook'),
           )
@@ -65,6 +84,7 @@ class _MyHomePageState extends State<MyHomePage> {
     // fast, so that you can just rebuild anything that needs updating rather
     // than having to individually change instances of widgets.
     return Scaffold(
+
       appBar: AppBar(
         // Here we take the value from the MyHomePage object that was created by
         // the App.build method, and use it to set our appbar title.
