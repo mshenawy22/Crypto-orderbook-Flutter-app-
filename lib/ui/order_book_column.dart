6be15d068573f2db1488cef15226594cddb2cfa9
jay shenawy <jay.shenawy@netxtra.net>
2022-01-17 00:49:31 +0000

    consuming live date and feeding into the order book

--- lib/ui/order_book_column.dart
+++ lib/ui/order_book_column.dart
@@ -1,5 +1,10 @@
+import 'package:cryptowatch_orderbook/cubit/prodct_list_cubit/product_list_cubit.dart';
+import 'package:cryptowatch_orderbook/models/api_response.dart';
+import 'package:cryptowatch_orderbook/services/kraken_api.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:intl/intl.dart';
 
 import 'orderbook_row.dart';
 import 'orderbook_row_titles.dart';
@@ -9,6 +14,7 @@ class OrderBookColumn extends StatelessWidget {
   final bool buyTable ;
   @override
   Widget build(BuildContext context) {
+
     return Expanded(
         child: Column(children: [
        Container(
@@ -34,7 +40,51 @@ class OrderBookColumn extends StatelessWidget {
                         color: Theme.of(context).canvasColor,
                         shape: Border.all(
                             width: 2.0, color: Theme.of(context).dividerColor)),
-                    child: OrderBookRow(price: 100, quantity: 10 * index , buyRow :buyTable));
+                    child:
+                        BlocConsumer<ProductListCubit, ProductListState>(
+                        listener: (context, state) {},
+                        builder: (context, state) {
+                          if (state.productList.isNotEmpty)
+                          {
+//                             1642373448597
+//                   print (state.productList.first.timestamp);
+//                             DateFormat formatter = DateFormat("dd MMMM yyyy");
+//                             print (DateTime.tryParse('1642374117560'));
+//                             return formatter.format(date);
+
+                            return OrderBookRow(
+                                price: state.productList.elementAt(index).price,
+                                quantity: state.productList.elementAt(index).qty,
+                                buyRow: buyTable);
+                          }
+                          else {
+                            return OrderBookRow(
+                                price: 0,
+                                quantity: 0,
+                                buyRow: buyTable);
+                          }
+                        }
+                        )
+
+                    // StreamBuilder <KrakenFutureResponse>(
+                    //     stream :  KrakenSocketApi.socketStream.asBroadcastStream() ,
+                    //     builder: (context,  snapshot)
+                    //     {
+                    //
+                    //       if (snapshot.hasData)
+                    //       {
+                    //
+                    //         return   OrderBookRow(price: snapshot.data!.price, quantity: 10.0 * index , buyRow :buyTable);
+                    //       }
+                    //       else {
+                    //         return Text('No data yet');
+                    //       }
+                    //     }
+                    //
+                    // )
+                );
+
+
 
                 //   Container(
                 //   height: 50,
