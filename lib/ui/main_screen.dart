2c712a607232ebd36c4262fd8e8d8c61dc7aa869
jay shenawy <jay.shenawy@netxtra.net>
2022-01-17 00:48:53 +0000

    Initializing KrakenSocketAPi

--- lib/ui/main_screen.dart
+++ lib/ui/main_screen.dart
@@ -1,28 +1,39 @@
+import 'package:cryptowatch_orderbook/cubit/prodct_list_cubit/product_list_cubit.dart';
+import 'package:cryptowatch_orderbook/models/api_response.dart';
+import 'package:cryptowatch_orderbook/services/kraken_api.dart';
 import 'package:cryptowatch_orderbook/ui/quantity_field.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 
 import 'order_book.dart';
 import 'order_book_column.dart';
 
-
-
-class MainAppScreen extends StatelessWidget {
-
+class MainAppScreen extends StatefulWidget {
   const MainAppScreen({Key? key}) : super(key: key);
 
   @override
+  _MainAppScreenState createState() => _MainAppScreenState();
+}
+
+class _MainAppScreenState extends State<MainAppScreen> {
+
+  @override
+  void initState() {
+
+    super.initState();
+    KrakenSocketApi(context);
+  }
+
+
+  @override
   Widget build(BuildContext context) {
-
-
     return Column(
       children: [
          Container( height: 20,),
         inputOrderField(),
         Container( height: 20,),
-        Expanded(child: OrderBook())
-
-
+        Expanded(child: OrderBook()),
       ],
 
     );
