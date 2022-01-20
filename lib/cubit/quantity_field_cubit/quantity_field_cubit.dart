eb1e428169316a4d577597694f86d550b9ba3ece
jay shenawy <jay.shenawy@netxtra.net>
2022-01-20 00:07:18 +0000

    cleanup

--- lib/cubit/quantity_field_cubit/quantity_field_cubit.dart
+++ lib/cubit/quantity_field_cubit/quantity_field_cubit.dart
@@ -3,10 +3,7 @@ import 'package:bloc/bloc.dart';
 part 'quantity_field_state.dart';
 
 class QuantityFieldCubit extends Cubit<QuantityFieldState> {
-  //on initialisation , go to the loading state (loaded = false)
   QuantityFieldCubit() : super(QuantityFieldState(0));
-
-
 
   void reset() => emit(QuantityFieldState(0));
 
