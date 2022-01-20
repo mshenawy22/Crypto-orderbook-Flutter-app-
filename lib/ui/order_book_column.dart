965593361c6a2f09879ac348e5759905342f9170
jay shenawy <jay.shenawy@netxtra.net>
2022-01-20 16:48:11 +0000

    Bloc consumer  to wrap the Listview builder to the items count comes from the cubit

--- lib/ui/order_book_column.dart
+++ lib/ui/order_book_column.dart
@@ -1,6 +1,4 @@
 import 'package:cryptowatch_orderbook/cubit/prodct_list_cubit/product_list_cubit.dart';
-import 'package:cryptowatch_orderbook/models/api_response.dart';
-import 'package:cryptowatch_orderbook/services/kraken_api.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
@@ -9,58 +7,65 @@ import 'orderbook_row.dart';
 import 'orderbook_row_titles.dart';
 
 class OrderBookColumn extends StatelessWidget {
-  const OrderBookColumn({Key? key , required this.buyTable}) : super(key: key);
-  final bool buyTable ;
+  const OrderBookColumn({Key? key, required this.buyTable}) : super(key: key);
+  final bool buyTable;
+
   @override
   Widget build(BuildContext context) {
-
     return Expanded(
         child: Column(children: [
-       Container(
+      Container(
           decoration: ShapeDecoration(
               color: Theme.of(context).canvasColor,
-
-          shape: Border(
-            top: BorderSide(color:Theme.of(context).dividerColor , width: 2.0 ),
-            bottom: BorderSide(color:Theme.of(context).dividerColor , width: 2.0 ),
-                   )
-          ),
+              shape: Border(
+                top: BorderSide(
+                    color: Theme.of(context).dividerColor, width: 2.0),
+                bottom: BorderSide(
+                    color: Theme.of(context).dividerColor, width: 2.0),
+              )),
           child: OrderBookRowTitle(
             buyTable: buyTable,
           )),
-          Expanded(
-          child: ListView.builder(
-              padding: const EdgeInsets.all(8),
-              itemCount: 10,
-              itemBuilder: (BuildContext context, int index) {
-                return Container(
-                    decoration: ShapeDecoration(
-                        color: Theme.of(context).canvasColor,
-                        shape: Border.all(
-                            width: 2.0, color: Theme.of(context).dividerColor)),
-                    child:
-                        BlocConsumer<ProductListCubit, ProductListState>(
-                        listener: (context, state) {},
-                        builder: (context, state) {
-                          if (state.productListBuySide.isNotEmpty && state.productListSellSide.isNotEmpty )
-                          {
-                            return OrderBookRow(
-                                price: buyTable ? state.productListBuySide.elementAt(index).price :state.productListSellSide.elementAt(index).price,
-                                quantity: buyTable? state.productListBuySide.elementAt(index).qty :state.productListSellSide.elementAt(index).qty ,
-                                buyRow: buyTable);
-                          }
-                          else {
-                            return OrderBookRow(
-                                price: 0,
-                                quantity: 0,
-                                buyRow: buyTable);
-                          }
-                        }
-                        )
-
-                );
-
-
+      Expanded(
+          child: BlocConsumer<ProductListCubit, ProductListState>(
+              listener: (context, state) {},
+              builder: (context, state) {
+                if (state.productListBuySide.isNotEmpty &&
+                    state.productListSellSide.isNotEmpty) {
+                  return ListView.builder(
+                      padding: const EdgeInsets.all(8),
+                      itemCount: buyTable? state.productListBuySide.length :  state.productListSellSide.length ,
+                      itemBuilder: (BuildContext context, int index) {
+                        return Container(
+                            decoration: ShapeDecoration(
+                                color: Theme.of(context).canvasColor,
+                                shape: Border.all(
+                                    width: 2.0,
+                                    color: Theme.of(context).dividerColor)),
+                            child: OrderBookRow(
+                                price: buyTable
+                                    ? state.productListBuySide
+                                        .elementAt(index)
+                                        .price
+                                    : state.productListSellSide
+                                        .elementAt(index)
+                                        .price,
+                                quantity: buyTable
+                                    ? state.productListBuySide
+                                        .elementAt(index)
+                                        .qty
+                                    : state.productListSellSide
+                                        .elementAt(index)
+                                        .qty,
+                                buyRow: buyTable));
+                      });
+                } else {
+                  return
+                    Center
+                  (
+                        child : Text('Loading...')
+                    );
+                }
               }))
     ]));
   }
