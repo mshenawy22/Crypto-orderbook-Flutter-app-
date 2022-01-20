9fb4d69e463d374cf4081a10fe22e6097ed46d47
jay shenawy <jay.shenawy@netxtra.net>
2022-01-20 16:44:30 +0000

    added new text style for order book titles

--- lib/main.dart
+++ lib/main.dart
@@ -31,16 +31,16 @@ class MyApp extends StatelessWidget {
             title:'CryptoWatch OrderBook',
             theme: ThemeData(
               // Define the default brightness and colors.
-              primaryColor:BLACKB ,
+              primaryColor:CWCOLORB ,
               brightness: Brightness.dark,
-              dividerColor: BLACKA,
-              scaffoldBackgroundColor:  BLACKB,
-              canvasColor: BLACKD,
+              dividerColor: CWCOLORA,
+              scaffoldBackgroundColor:  CWCOLORB,
+              canvasColor: CWCOLORD,
               primaryTextTheme: const TextTheme(
                 headline1: TextStyle(fontSize: 72.0, fontWeight: FontWeight.bold),
-                headline6: TextStyle(fontSize: 36.0, fontStyle: FontStyle.italic),
                 headline4: TextStyle(fontSize: 14.0, fontStyle: FontStyle.italic),
                 bodyText1: TextStyle(fontSize: 14.0, fontFamily: 'Hind'),
+                  subtitle1 :TextStyle(color : CWCOLORE ,decoration :TextDecoration.underline  , decorationStyle : TextDecorationStyle.dotted ,decorationThickness: 3 )
               ),
 
               // Define the default font family.
@@ -63,14 +63,6 @@ class MyApp extends StatelessWidget {
 class MyHomePage extends StatefulWidget {
   const MyHomePage({Key? key, required this.title}) : super(key: key);
 
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
 
   final String title;
 
@@ -83,17 +75,11 @@ class _MyHomePageState extends State<MyHomePage> {
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
+
     return Scaffold(
 
       appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
+
         title: Text('CryptoWatch BTCUSD OrderBook '),
       ),
       body:MainAppScreen ()
