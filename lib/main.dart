62870d4667798e1ecf58e6adffb7ab6632148f6d
jay shenawy <jay.shenawy@netxtra.net>
2022-01-20 00:07:31 +0000

    adding bodytext1

--- lib/main.dart
+++ lib/main.dart
@@ -40,7 +40,7 @@ class MyApp extends StatelessWidget {
                 headline1: TextStyle(fontSize: 72.0, fontWeight: FontWeight.bold),
                 headline6: TextStyle(fontSize: 36.0, fontStyle: FontStyle.italic),
                 headline4: TextStyle(fontSize: 14.0, fontStyle: FontStyle.italic),
-                bodyText2: TextStyle(fontSize: 14.0, fontFamily: 'Hind'),
+                bodyText1: TextStyle(fontSize: 14.0, fontFamily: 'Hind'),
               ),
 
               // Define the default font family.
